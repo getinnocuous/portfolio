e0216edcb725a1bfeae1e49421e79ac5acca92b2
Elliot Waddington <elliot.waddington@autotrader.co.uk>
2020-10-30 15:00:22 +0000

    grid columns fix

--- components/SkillGrid/SkillGrid.js
+++ components/SkillGrid/SkillGrid.js
@@ -4,8 +4,11 @@ const Grid = styled.div`
   display: grid;
   padding: 1rem;
   grid-gap: 1rem;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   margin-bottom: var(--h-spacing);
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  }
   &:last-of-type {
     margin-bottom: 0;
   }
